41fdd924aed560b23244503e13d3af45c57d9bba
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-07 19:08:05 +0100

    Update error handling for tests [skip ci]

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -45,7 +45,7 @@ func TestApplySingleDisk(t *testing.T) {
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+	defer terraform.DestroyE(t, terraformOptions)
 
 	// At the end of the test, run `terraform destroy` again, in case failures leave orphaned resources
 	defer terraform.Destroy(t, terraformOptions)
@@ -90,7 +90,7 @@ func TestApplyMultiDisk(t *testing.T) {
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+	defer terraform.DestroyE(t, terraformOptions)
 
 	// At the end of the test, run `terraform destroy` again, in case failures leave orphaned resources
 	defer terraform.Destroy(t, terraformOptions)
